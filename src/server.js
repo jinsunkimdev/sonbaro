9f061c5b119a16555cbb6732acf6acedae9d79cc
jinsunkimdev <jinsunkim94@gmail.com>
2023-06-17 21:49:36 +0900

    User login

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./routers/globalRouter.js";
 import restaurantRouter from "./routers/restaurantRouter.js";
 import userRouter from "./routers/userRouter.js";
+import { localsMiddleware } from "./middlewares.js";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +15,15 @@ app.set('views', process.cwd() + "/src/views");
 //Middleware
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+      secret: "Hello!",
+      resave: false,
+      saveUninitialized: false,
+      store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI }),
+    })
+  );
+app.use(localsMiddleware);
 //Router
 app.use("/", globalRouter);
 app.use("/restaurants", restaurantRouter);
